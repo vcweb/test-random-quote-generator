cf978ba2c752376cc642a63e3fb55201e5236ea7
Tom Zhang <tom-zhang@hotmail.co.uk>
2017-11-21 12:53:55 +0000

    edit files

--- test random quote generator/server.js
+++ test random quote generator/server.js
@@ -1,68 +1,49 @@
 'use strict';
 
-var pomodoro = {
-	started: false,
-	minutes: 0,
-	seconds: 0,
-	interval: null,
-	minutesDOM: null,
-	secondsDOM: null,
-	init: function () {
-		var self = this;
-		this.minutesDOM = document.querySelector('#minutes');
-		this.secondsDOM = document.querySelector('#seconds');
-		this.interval = setInterval(function () {
-			self.intervalCallback.apply(self);
-		}, 1000);
-		document.querySelector('#start').onclick = function () {
-			self.startTimer.apply(self);
-		}; document.querySelector('#stop').onclick = function () {
-			self.stopTimer.apply(self);
-		}; document.querySelector('#reset').onclick = function () {
-			self.resetTimer.apply(self);
-		};
-	},
-	resetVariables(mins, secs, started) {
-		this.minutes = mins;
-		this.seconds = secs;
-		this.started = started;
-	},
-	startTimer: function () {
-		this.resetVariables(25, 0, true);
 
-	},
-	resetTimer: function () {
-		this.resetVariables(25, 0, false);
-		this.updateDOM();
-	},
-	stopTimer: function () {
-		this.resetVariables(false);
-		//this.updateDOM();    
-	},
-	updateDOM: function () {
-		this.minutesDOM.innerHTML = this.minutes;
-		this.secondsDOM.innerHTML = this.seconds;
+function quote() {
+	var quoteData = ["Everybody is special. Everybody. Everybody is a hero, a lover, a fool, a villain. Everybody. Everybody has their story to tell.", "If a man does his best, what else is there?", "Success is most often achieved by those who don't know that failure is inevitable."];
+	var authorData = ["Alan Moore, V for Vendetta", "Gen. George S. Patton", "Coco Chanel"];
 
-	},
-	intervalCallback: function () {
-		if (!this.started) return false;
-		if (this.seconds == 0) {
-			if (this.minutes == 0) {
-				this.timerComplete();
-				return;
-			}
-			this.seconds = 59;
-			this.minutes--;
-		} else {
-			this.seconds--;
-		}
-		this.updateDOM();
-	},
-	timerComplete: function () {
-		this.started = false;
+	var quoteNumber = randomInteger(quoteData.length, currentNumber)
+
+	var quoteBody = quoteData[quoteNumber]
+	var quoteAuthor = authorData[quoteNumber]
+
+	currentNumber = quoteNumber;
+
+	document.getElementById("quoteDesign").innerHTML = quoteBody
+	document.getElementById("authorDesign").innerHTML = quoteAuthor
+
+	//using encodeURIComponent to make sure special chars in the text does't get cut
+	var quoteBodyURI = encodeURIComponent(quoteBody)
+	var quoteAuthorURI = encodeURIComponent(quoteAuthor)
+
+	/* document.getElementById("btnTweet").href = 'https://twitter.com/intent/tweet?text=' + quoteBodyURI + ' - ' + quoteAuthorURI  */
+	$('.btnTweet').on("click", function () {
+		var url = 'https://twitter.com/intent/tweet?text=' + quoteBodyURI + ' - ' + quoteAuthorURI;
+		window.open(url, 'twitter');
+		return false;
+	});
+}
+
+//random integer generator
+function randomInteger(totalQuotes, prevNumber) {
+	var number = Math.floor(Math.random() * totalQuotes)
+	if (prevNumber === null) {
+		return number;
 	}
+	while (number === prevNumber) {
+		number = Math.floor(Math.random() * totalQuotes)
+	}
+	return number;
 }
 
-window.onload = function () {
-	pomodoro.init();
-}
+var currentNumber = null;
+
+window.onload = quote;
+
+$('.inspire').on("click", function () {
+	quote();
+});
+
